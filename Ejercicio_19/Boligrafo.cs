b5ac09b7e510da95645574dcf96ae4b7f7ddf13c
RomeroFederico <romerof14@gmail.com>
2016-04-01 09:44:12 -0300

    Agregados metodos y getters en la clase Boligrafo.
    . Pintar().
    . Recargar().

--- Ejercicio_19/Boligrafo.cs
+++ Ejercicio_19/Boligrafo.cs
@@ -7,5 +7,41 @@ namespace Ejercicio_19.Boligrafo
 {
     class Boligrafo
     {
+        private ConsoleColor _color;
+        private int tinta;
+
+        public Boligrafo(ConsoleColor color, int tinta)
+        {
+            this._color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this._color;
+        }
+
+        public int GetTinta()
+        {
+            return this.tinta;
+        }
+
+        public bool Pintar(int gasto)
+        {
+            if (gasto > this.GetTinta())
+            {
+                Console.Write("No hay tinta suficiente");
+                return false;
+            }
+            Console.ForegroundColor = this.GetColor();
+            Console.WriteLine("Pintando... ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            return true;
+        }
+
+        public void Recargar()
+        {
+            this.tinta += 100;
+        }
     }
 }
