d649752f0e6ffb829f2c22eb73cc4f10aaf58912
RomeroFederico <romerof14@gmail.com>
2016-04-01 09:32:44 -0300

    Main y clase Boligrafo creadas.

---
+++ Ejercicio_19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ejercicio_19.Boligrafo;
+
+namespace Ejercicio_19
+{
+
+    /*   
+    19.  Crear la clase Bolígrafo. 
+ 
+    Generar sólo los métodos getters para ambos atributos.  
+    En el Main, crear un bolígrafo de tinta azul (ConsoleColor.Blue) 
+    y una cantidad inicial de tinta de 100 (cantidad máxima para 
+    todos los bolígrafos) y otro de tinta roja (ConsoleColor.Red) y 
+    50 de tinta. Utilizar todos los métodos en el Main.  
+ 
+    Nota: Crear el constructor que crea conveniente. La clase 
+    Boligrafo y el Program deben estar en namespaces distintos.
+    */
+
+    class Ejercicio_19
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. 19";
+        }
+    }
+}
